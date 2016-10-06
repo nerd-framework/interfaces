61af3b39a88d86a549c83c2711c7d87a962e5b43
Roman Gemini <roman@homefs.biz>
2016-10-06 11:35:09 +0300

    add isMethod method @ RequestContract

--- src/Nerd/Framework/Http/RequestContract.php
+++ src/Nerd/Framework/Http/RequestContract.php
@@ -4,6 +4,25 @@ namespace Nerd\Framework\Http;
 
 interface RequestContract
 {
+    /**
+     * Get HTTP Request method.
+     *
+     * @return string
+     */
     public function getMethod();
+
+    /**
+     * Test that $method equals to HTTP Request method.
+     *
+     * @param string $method
+     * @return mixed
+     */
+    public function isMethod($method);
+
+    /**
+     * Get HTTP Request path.
+     *
+     * @return string
+     */
     public function getPath();
 }
