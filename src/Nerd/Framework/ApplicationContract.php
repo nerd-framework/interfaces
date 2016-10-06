3d7f48a7377ac938c27af2d329c3e9948e4c8d2e
Roman Gemini <roman@homefs.biz>
2016-10-06 16:09:06 +0300

    add ApplicationContract

---
+++ src/Nerd/Framework/ApplicationContract.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nerd\Framework;
+
+use Nerd\Framework\Http\RequestContract;
+use Nerd\Framework\Http\ResponseContract;
+
+interface ApplicationContract
+{
+    /**
+     * Handle HTTP Request by Application and return HTTP Response.
+     *
+     * @param RequestContract $request
+     * @return ResponseContract
+     */
+    public function handle(RequestContract $request);
+}
