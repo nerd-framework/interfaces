8f28f6a6eea7cb5c42c6c6627b068517b25d0e13
Roman Gemini <roman@homefs.biz>
2016-10-06 14:44:10 +0300

    extend RequestContract

--- src/Nerd/Framework/Http/RequestContract.php
+++ src/Nerd/Framework/Http/RequestContract.php
@@ -25,4 +25,40 @@ interface RequestContract
      * @return string
      */
     public function getPath();
+
+    /**
+     * Get server parameter.
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    public function getServerParameter($key, $default = null);
+
+    /**
+     * Get query string parameter.
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    public function getQueryParameter($key, $default = null);
+
+    /**
+     * Get post parameter.
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    public function getPostParameter($key, $default = null);
+
+    /**
+     * Get cookie.
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    public function getCookie($key, $default = null);
 }
