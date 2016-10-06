e70e12786e9e8ce0485ae20f28381ba535eb2ddc
Roman Gemini <roman@homefs.biz>
2016-10-06 14:40:25 +0300

    add InputContract

---
+++ src/Nerd/Framework/Http/InputContract.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Nerd\Framework\Http;
+
+interface InputContract
+{
+    /**
+     * Get HTTP Request Object.
+     *
+     * @return RequestContract
+     */
+    public function getRequestObject();
+
+    /**
+     * Get HTTP Request Body as string.
+     *
+     * @return string
+     */
+    public function getRequestBody();
+
+    /**
+     * Get HTTP Request Body Stream.
+     *
+     * @return resource
+     */
+    public function getRequestBodyStream();
+}
