ff80a028f279c5f851b19f04bb969d097a1e7458
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2016-10-08 18:18:57 +0300

    add Service contracts

---
+++ src/Nerd/Framework/Services/ServiceContract.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Nerd\Framework\Services;
+
+interface ServiceContract
+{
+    //
+}
---
+++ src/Nerd/Framework/Services/ServiceProviderContract.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Nerd\Framework\Services;
+
+interface ServiceProviderContract
+{
+    public function register();
+}
