531d4f5c658647e9e3eda8ce5c243be2c94499f5
Roman <roman@homefs.biz>
2016-10-12 20:21:00 +0300

    add couple of methods

--- src/Nerd/Framework/Http/Request/RequestContract.php
+++ src/Nerd/Framework/Http/Request/RequestContract.php
@@ -27,6 +27,13 @@ interface RequestContract
     public function getPath();
 
     /**
+     * Is HTTPS connection used.
+     *
+     * @return boolean
+     */
+    public function isSecure();
+
+    /**
      * Get server parameter.
      *
      * @param string $key
@@ -61,4 +68,25 @@ interface RequestContract
      * @return mixed
      */
     public function getCookie($key, $default = null);
+
+    /**
+     * Get user agent.
+     *
+     * @return mixed
+     */
+    public function getUserAgent();
+
+    /**
+     * Get remote address.
+     *
+     * @return mixed
+     */
+    public function getRemoteAddress();
+
+    /**
+     * Get local address.
+     *
+     * @return mixed
+     */
+    public function getServerAddress();
 }
