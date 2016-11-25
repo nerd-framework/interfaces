81d0ab0af0c5fc61212382e4685160a6d819f6c6
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2016-11-25 21:57:29 +0200

    Add RoutePatternMatcherContract.

---
+++ src/Nerd/Framework/Routing/RoutePatternMatcher/RoutePatternMatcherContract.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 11/25/16
+ * Time: 5:35 PM
+ */
+
+namespace Nerd\Framework\Routing\RoutePatternMatcher;
+
+interface RoutePatternMatcherContract
+{
+    public function matches(string $route): boolean;
+
+    public function parameters(string $route): array;
+}
