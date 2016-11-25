6951f53c886125f2c59f1cc514ef8af2792b8f7f
Roman Gemini <roman@homefs.biz>
2016-11-25 15:01:33 +0200

    Extend service provider contract.

--- src/Nerd/Framework/Services/ServiceProviderContract.php
+++ src/Nerd/Framework/Services/ServiceProviderContract.php
@@ -4,7 +4,24 @@ namespace Nerd\Framework\Services;
 
 interface ServiceProviderContract
 {
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot();
+
+    /**
+     * Register service provider.
+     *
+     * @return void
+     */
     public function register();
 
-    public static function provides();
+    /**
+     * List of services provided by this provider.
+     *
+     * @return mixed
+     */
+    public function provides();
 }
