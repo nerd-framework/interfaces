ce96a2771e180d884cd38f7e4b188b42a7005db9
Roman Gemini <roman@homefs.biz>
2016-11-29 09:57:22 +0200

    Update contracts for new router version.

--- src/Nerd/Framework/Routing/RouterContract.php
+++ src/Nerd/Framework/Routing/RouterContract.php
@@ -10,69 +10,54 @@ interface RouterContract
      * Add route for GET method into routes list.
      *
      * @param string $pattern
-     * @param callable $action
-     * @param mixed $data
+     * @param callable[] ...$action
      * @return RouterContract
      */
-    public function get(string $pattern, callable $action, $data = null);
+    public function get(string $pattern, callable ...$action);
 
     /**
      * Add route for POST method into routes list.
      *
      * @param string $pattern
-     * @param callable $action
-     * @param mixed $data
+     * @param callable[] ...$action
      * @return RouterContract
      */
-    public function post(string $pattern, callable $action, $data = null);
+    public function post(string $pattern, callable ...$action);
 
     /**
      * Add route for PUT method into routes list.
      *
      * @param string $pattern
-     * @param callable $action
-     * @param mixed $data
+     * @param callable[] ...$action
      * @return RouterContract
      */
-    public function put(string $pattern, callable $action, $data = null);
+    public function put(string $pattern, callable ...$action);
 
     /**
      * Add route for DELETE method into routes list.
      *
      * @param string $pattern
-     * @param callable $action
-     * @param mixed $data
+     * @param callable[] ...$action
      * @return RouterContract
      */
-    public function delete(string $pattern, callable $action, $data = null);
+    public function delete(string $pattern, callable ...$action);
 
     /**
      * @param string $pattern
-     * @param callable $action
-     * @param null $data
+     * @param callable[] ...$action
      * @return RouterContract
      */
-    public function any(string $pattern, callable $action, $data = null);
-
-    /**
-     * Add middleware to router.
-     *
-     * @param string $pattern
-     * @param callable $middleware
-     * @return RouterContract
-     */
-    public function middleware(string $pattern, callable $middleware);
+    public function any(string $pattern, callable ...$action);
 
     /**
      * Add route into routes list.
      *
      * @param array $methods
      * @param string $pattern
-     * @param callable $action
-     * @param mixed $data
+     * @param callable[] ...$action
      * @return RouterContract
      */
-    public function add(array $methods, string $pattern, callable $action, $data = null);
+    public function add(array $methods, string $pattern, callable ...$action);
 
     /**
      * Pass request through router.
